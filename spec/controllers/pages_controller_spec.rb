569e626deea0486fa166b97538b9345adc0a4ff3
ErikPeterson <eriksalgstrom@gmail.com>
2014-05-09 11:47:32 -0400

    Add failing spec for index action.

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe PagesController do
-
+	describe "#index" do
+		it "should render the root route" do
+			get :index
+			expect(response).to render_template("index")
+		end
+	end
 end
