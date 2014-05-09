4785b10442a3b5c897bb46b285f5303118ba2e6e
ErikPeterson <eriksalgstrom@gmail.com>
2014-05-09 11:34:48 -0400

    Add initial spec for User covering attribute validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,46 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "::create" do
+  	describe "validations" do
+	  	before :each do
+	  		@user = User.new().tap{|u|
+	  			u.email = "test@test.com"
+	  			u.name = "Dr. Test McTest"
+	  			u.title = "Psychiatrist"
+	  			u.degree = "PhD"
+	  			u.address = "129 West 81st St. #5A, New York, New York, 10024"
+	  			u.password = "Testing123!"
+	  			u.password_confirmation = "Testing123!"
+	  		}
+	  	end
+	  	it "will save with valid values for all attributes" do
+	  		expect(@user.save).to equal(true)
+	  	end
+	  	it "cannot have a blank email" do
+	  		@user.email = ""
+	  		expect(@user.save).to equal(false)
+	  	end
+	  	it "must have a valid email" do
+	  		@user.email = "Mr. Bogus"
+	  		expect(@user.save).to equal(false)
+	  	end
+	  	it "cannot have a blank name" do
+	  		@user.name = ""
+	  		expect(@user.save).to equal(false)
+	  	end
+	  	it "cannot have a blank address" do
+	  		@user.address = ""
+	  		expect(@user.save).to equal(false)
+	  	end
+	  	it "cannot have a blank degree" do
+	  		@user.degree = ""
+	  		expect(@user.save).to equal(false)
+	  	end
+	  	it "cannot have a blank title" do
+	  		@user.title = ""
+	  		expect(@user.save).to equal(false)
+	  	end
+	end
+  end
 end
