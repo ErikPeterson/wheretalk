2b005c80edb2b1fc66c921c0998a72bf64f03c6e
ErikPeterson <eriksalgstrom@gmail.com>
2014-05-11 15:13:02 -0400

    Add four doctors to seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+users = User.create([
+	{
+		name: "Dr. Peter Von Nostrand",
+		email: "petervn@att.com",
+		title: "Psychiatrist",
+		degree: "PhD",
+		address: "81 West 81st St. New York, NY",
+		password: "cosmokramer",
+		password_confirmation: "comsmokramer"
+	},{
+		name: "Art Vandelay",
+		email: "boscolover@comcast.net",
+		title: "Licensed Social Worker",
+		degree: "MSW",
+		address: "158 West 84st St. New York, NY",
+		password: "georgecostanza",
+		password_confirmation: "georgecostanza"
+	},{
+		name: "Dr. Kel Varnson",
+		email: "kel_varnson@nyumed.edu",
+		title: "Psychotherapist",
+		degree: "PsyD",
+		address: "158 West 84st St. New York, NY",
+		password: "jerryseinfeld",
+		password_confirmation: "jerryseinfeld"
+	},{
+		name: "Dr. Paloma Benes",
+		email: "drbenes@gmail.com",
+		title: "Behavorial Therapist",
+		degree: "PsyD",
+		address: "158 West 84st St. New York, NY",
+		password: "jerryseinfeld",
+		password_confirmation: "jerryseinfeld"
+	}
+
+	]);
